b94ebc657383b6635f16a94ca84092fd6d5d6a40
moooc <389094993@qq.com>
2020-03-28 14:11:13 +0800

    update main.lua

--- ActinidiaWin32/game/lua/main.lua
+++ ActinidiaWin32/game/lua/main.lua
@@ -3,10 +3,12 @@
 |            DO NOT MODIFY             |
 +======================================+
 ]]
--- entrance
 core = load(GetText("lua/core.lua"))()
-
 local custom = nil
+
+--[[
+Load resources here.
+]]
 function OnCreate()
     script = load(GetText("lua/001.lua"))
     if not script then 
@@ -17,6 +19,9 @@ function OnCreate()
     return ""
 end
 
+--[[
+Draw anything here.
+]]
 function OnPaint(WndGraphic)
     local next_script = custom.OnPaint(WndGraphic)
     if next_script ~= "" then
@@ -29,38 +34,84 @@ function OnPaint(WndGraphic)
     end
 end
 
+--[[
+Free resources that you created here.
+]]
 function OnClose()
     custom.OnClose()
 end
 
+--[[
+The function is called when player pressed any key.
+@param nChar The key id pressed by player. See `core.lua` for more infomation.
+]]
 function OnKeyDown(nChar)
     custom.OnKeyDown(nChar)
 end
 
+--[[
+The function is called when player released any key.
+@param nChar The key id released by player. See `core.lua` for more infomation.
+]]
 function OnKeyUp(nChar)
     custom.OnKeyUp(nChar)
 end
 
+--[[
+The function is called when player touch the screen or clicked the window.
+@param x   If `preferred_width` is set in `config.ini`,
+           the range is [0, preferred_width], else [0, core.screen_width]
+@param y   If `preferred_height` is set in `config.ini`,
+           the range is [0, preferred_height], else [0, core.screen_height]
+]]
 function OnLButtonDown(x,y)
     custom.OnLButtonDown(x,y)
 end
 
+--[[
+The function is called when player released the thumb or mouse left button.
+@param x   If `preferred_width` is set in `config.ini`,
+           the range is [0, preferred_width], else [0, core.screen_width]
+@param y   If `preferred_height` is set in `config.ini`,
+           the range is [0, preferred_height], else [0, core.screen_height]
+]]
 function OnLButtonUp(x,y)
     custom.OnLButtonUp(x,y)
 end
 
+--[[
+The function is called when player move thumb or mouse on screen.
+@param x   If `preferred_width` is set in `config.ini`,
+           the range is [0, preferred_width], else [0, core.screen_width]
+@param y   If `preferred_height` is set in `config.ini`,
+           the range is [0, preferred_height], else [0, core.screen_height]
+]]
 function OnMouseMove(x,y)
     custom.OnMouseMove(x,y)
 end
 
+--[[
+When the window get focus, the function is called.
+]]
 function OnSetFocus()
     custom.OnSetFocus()
 end
 
+--[[
+When the window lost focus, the function is called.
+]]
 function OnKillFocus()
     custom.OnKillFocus()
 end
 
+--[[
+When the user scroll the mouse wheel, the function is called.
+@param zDelta  A relative value of mouse wheel
+@param x       If `preferred_width` is set in `config.ini`,
+               the range is [0, preferred_width], else [0, core.screen_width]
+@param y       If `preferred_height` is set in `config.ini`,
+               the range is [0, preferred_height], else [0, core.screen_height]
+]]
 function OnMouseWheel(zDeta,x,y)
     custom.OnMouseWheel(zDeta,x,y)
 end
